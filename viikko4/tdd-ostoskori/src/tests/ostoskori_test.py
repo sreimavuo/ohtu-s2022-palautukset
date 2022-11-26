83f1f8d10c5a34236f45aeed15d605b2e3b148b6
Sami Reimavuo <sami@reimavuo.net>
2022-11-26 20:31:32 +0200

    Viikko 4, tehtävä 5, step 1 valmis

---
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -0,0 +1,13 @@
+import unittest
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+class TestOstoskori(unittest.TestCase):
+    def setUp(self):
+        self.kori = Ostoskori()
+
+    # Step 1
+    def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+    
