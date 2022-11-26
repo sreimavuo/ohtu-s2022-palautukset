b93ebf8b31e8780aa047c241e91baccca44b0d67
Sami Reimavuo <sami@reimavuo.net>
2022-11-26 22:21:36 +0200

    Viikko 4, tehtävä 5, step 6 tehty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -44,6 +44,12 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 5)
 
     # Step 6
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
     # Step 7
 
